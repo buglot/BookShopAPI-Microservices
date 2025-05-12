f3df559035dc62f2ad086000089d4b1efc7961aa
buglot <ptii2x@gmail.com>
2025-05-12 20:44:02 +0700

    Add project files.

---
+++ BookShop.Services.CouponAPI/Model/Coupon.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShop.Services.CouponAPI.Model
+{
+    public class Coupon
+    {
+        public int CouponId { get; set; }
+        public required string CouponCode { get; set; }
+        public double DiscountAmount    { get; set; }
+        public int MinAmount { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
---
+++ BookShop.Services.CouponAPI/Model/Dto/CouponDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShop.Services.CouponAPI.Model.Dto
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        public required string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
